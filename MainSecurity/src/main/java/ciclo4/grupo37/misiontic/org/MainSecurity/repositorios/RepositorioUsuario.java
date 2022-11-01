4022a744246041ff0556db68aacf078aca2a3185
diego-debian <profediegoparra01@gmail.com>
2022-10-31 21:58:30 -0500

    Usuario terminado

--- MainSecurity/src/main/java/ciclo4/grupo37/misiontic/org/MainSecurity/repositorios/RepositorioUsuario.java
+++ MainSecurity/src/main/java/ciclo4/grupo37/misiontic/org/MainSecurity/repositorios/RepositorioUsuario.java
@@ -1,4 +1,13 @@
 package ciclo4.grupo37.misiontic.org.MainSecurity.repositorios;
 
-public interface RepositorioUsuario {
+import ciclo4.grupo37.misiontic.org.MainSecurity.modelos.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RepositorioUsuario extends MongoRepository<Usuario, String> {
+    @Query("{'correo':?0}")
+    public Usuario getUserByMail(String correo);
+
+    @Query("{'seudonimo':?0}")
+    public Usuario getUserBySeudonimo(String seudonimo);
 }
