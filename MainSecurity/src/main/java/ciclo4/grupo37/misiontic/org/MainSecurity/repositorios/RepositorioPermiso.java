b954ad97db4f2347e54c5307e67aedf411d61848
diego-debian <profediegoparra01@gmail.com>
2022-11-02 22:30:28 -0500

    Cambios en repositorio Permiso en el query

--- MainSecurity/src/main/java/ciclo4/grupo37/misiontic/org/MainSecurity/repositorios/RepositorioPermiso.java
+++ MainSecurity/src/main/java/ciclo4/grupo37/misiontic/org/MainSecurity/repositorios/RepositorioPermiso.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface RepositorioPermiso  extends MongoRepository<Permiso, String> {
-    @Query("{'url=?0','metodo=?1'}")
+    @Query("{'url':?0,'metodo':?1}")
     Permiso getPermiso(String url, String metodo);
 }
